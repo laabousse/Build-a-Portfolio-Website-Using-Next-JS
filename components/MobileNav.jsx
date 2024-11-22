2a50416963b951d6f33094539d291c406e2e9fc3
laabousse <idriss.laabousse@gmail.com>
2024-11-22 20:02:47 +0800

    fixMobileNav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,11 +1,14 @@
 "use client";
+
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
-import { DialogTitle } from "@radix-ui/react-dialog"; // Import DialogTitle
-import { DialogDescription } from "@radix-ui/react-dialog"; // Import DialogDescription
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden"; // Import VisuallyHidden
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { useState } from "react";
+
 const links = [
   {
     name: "home",
@@ -28,41 +31,44 @@ const links = [
     path: "/contact",
   },
 ];
+
 const MobileNav = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <Sheet>
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
-        {/* DialogTitle wrapped in VisuallyHidden for screen reader accessibility */}
         <VisuallyHidden>
           <DialogTitle>Mobile Navigation</DialogTitle>
         </VisuallyHidden>
-        {/* DialogDescription for screen reader description */}
         <VisuallyHidden>
           <DialogDescription>
             Here you can find the navigation links for the mobile version of the
             website.
           </DialogDescription>
         </VisuallyHidden>
-        {/*logo*/}
         <div className="mt-32 mb-40 text-center text-2xl">
-          <Link href="/">
+          <Link href="/" onClick={handleLinkClick}>
             <h1 className="text-2xl font-semibold">
               Laabousse<span className="text-accent">.</span>
             </h1>
           </Link>
         </div>
-        {/*navigation*/}
         <nav className="flex flex-col justify-center items-center gap-6">
           {links.map((link, index) => {
             return (
               <Link
                 href={link.path}
                 key={index}
+                onClick={handleLinkClick}
                 className={`${
                   link.path === pathname &&
                   "text-accent border-b-2 border-accent"
